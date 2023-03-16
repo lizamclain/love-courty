c93c78a3f39306cb1109618aca21d419607a751f
Liza McLain <lizamclain@gmail.com>
2023-03-16 12:06:32 -0500

    search works

--- client/src/components/BrowseParks.js
+++ client/src/components/BrowseParks.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import NavBar from './NavBar';
 
 import ParkList from './ParkList'
+import Search from './Search'
 
 export default function BrowseParks({ setParkId, updateUser }) {
     const [parks, setParks] = useState([])
+    const [searchText, setSearchText] = useState("")
+
 
     useEffect (() => {
         fetch('/parks')
@@ -13,12 +16,18 @@ export default function BrowseParks({ setParkId, updateUser }) {
     },[])
     // console.log(parks)
 
+    const handleSearchInput = (e) => {
+        setSearchText(e.target.value)
+    }
+
+    const searchedParks = parks.filter(park => park.name.toLowerCase().includes(searchText.toLowerCase()))
 
     return (
         <div>
             <NavBar updateUser={updateUser}></NavBar>
+            <Search searchText={searchText} handleSearchInput={handleSearchInput}/>
             <h1>Browse all Parks</h1>
-            <ParkList parks={parks} setParkId={setParkId}/>
+            <ParkList parks={searchedParks} setParkId={setParkId}/>
         </div>
     )
 }
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Search() {
+export default function Search( {searchText, handleSearchInput} ) {
+
     return (
         <div>
             <h1>Search</h1>
+            <div className="searchbar">
+                <label htmlFor="search">Search Parks: </label>
+                    <input
+                        type="text"
+                        id="search"
+                        placeholder="Type a park to search..."
+                        value={searchText}
+                        onChange={handleSearchInput}
+                        // onChange={(e) => console.log("Searching...")}
+                    />
+            </div>
         </div>
     )
 }
