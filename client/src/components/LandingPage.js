d97e7e995af42533c36146b10d95cbf017b3537f
Liza McLain <lizamclain@gmail.com>
2023-03-22 17:20:07 -0500

    landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from "semantic-ui-react";
+// import Button from 'react-bootstrap/Button';
+
+import { Button, Stack } from 'react-bootstrap'
 
 
 export default function LandingPage() {
@@ -13,11 +15,13 @@ export default function LandingPage() {
         navigate('/signup')
     }
     return (
-        <div>
-            <h1>Welcome to Love-Courty! 🎾</h1>
-            <h3><em>Your go-to app to reserve a court in your area</em></h3>
-            <Button inverted color='blue' onClick={handleLoginBtnClick}>Login</Button>
-            <Button inverted color='blue' onClick={handleSignupBtnClick}>Signup</Button>
-        </div>
+        <Stack gap={2}>
+            <h1 class="d-flex justify-content-center">Welcome to Love-Courty! 🎾</h1>
+            <h3 class="d-flex justify-content-center"><em>Your go-to app to reserve a court in your area</em></h3>
+            <Stack direction="horizontal" gap={2} class="col-md-5 mx-auto">
+                <Button id='regular-btn' variant="outline-primary" onClick={handleLoginBtnClick}>Login</Button>
+                <Button id='regular-btn' onClick={handleSignupBtnClick}>Signup</Button>
+            </Stack>
+        </Stack>
     )
 }
