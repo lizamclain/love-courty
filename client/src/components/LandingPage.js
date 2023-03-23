a487048e61eddff8d6e8f26818e589f12bfa96d1
Liza McLain <lizamclain@gmail.com>
2023-03-23 10:25:21 -0500

    login page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
-
 import { Button, Stack } from 'react-bootstrap'
 
 
@@ -15,13 +13,17 @@ export default function LandingPage() {
         navigate('/signup')
     }
     return (
-        <Stack gap={2}>
-            <h1 class="d-flex justify-content-center">Welcome to Love-Courty! 🎾</h1>
-            <h3 class="d-flex justify-content-center"><em>Your go-to app to reserve a court in your area</em></h3>
-            <Stack direction="horizontal" gap={2} class="col-md-5 mx-auto">
+        <div class="mx-auto">
+            <Stack gap={2}>
+                <h1 class="d-flex justify-content-center">Welcome to Love-Courty! 🎾</h1>
+                <h3 class="d-flex justify-content-center"><em>Your go-to app to reserve a court in your area</em></h3>
+            <Stack gap={2} class="d-flex justify-content-center">
                 <Button id='regular-btn' variant="outline-primary" onClick={handleLoginBtnClick}>Login</Button>
                 <Button id='regular-btn' onClick={handleSignupBtnClick}>Signup</Button>
             </Stack>
-        </Stack>
+            </Stack>
+        </div>
     )
 }
+
+
